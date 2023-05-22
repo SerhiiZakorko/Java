51d80a1b16999b8d30d0dcdcb72ed3dd858eb895
SerhiiZakorko <129493695+SerhiiZakorko@users.noreply.github.com>
2023-05-22 16:03:16 +0200

    Add files via upload
    Новый вариант с использованием Scanner и на одной странице

--- HW_22_05/Box.java
+++ HW_22_05/Box.java
@@ -3,17 +3,22 @@ package HW;
 import java.util.Scanner;
 
 public class Box {
+    float width;
+    float high;
+    float dept;
+    public Box (float width, float high, float dept){
+        this.width = width;
+        this.high = high;
+        this.dept = dept;
 
-        float width;
-        float high;
-        float depth;
+        System.out.println("Объем коробки равен " + width*high*dept);
+    }
 
-        public Box(float width, float high, float depth){
-            this. width = width;
-            this.high = high;
-            this.depth = depth;
-
-            System.out.println("Объем коробки равен: " + width*high*depth);
-        }
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите ширину, высоту и глубину коробки: ");
+        new Box(sc.nextFloat(), sc.nextFloat(), sc.nextFloat());
+        sc.close();
+    }
 }
+
